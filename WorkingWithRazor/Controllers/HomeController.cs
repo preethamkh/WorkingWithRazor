1bc4973e883bc224c0573b79fde83c8b8bdf4f88
preethamkh <iudpgfn8>
2016-12-18 22:07:55 +1100

    missing files, controllers etc. added

---
+++ WorkingWithRazor/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace WorkingWithRazor.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var names = new[] {"Apple", "Orange", "Pear"};
+            return View(names);
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Time()
+        {
+            return PartialView(DateTime.Now);
+        }
+
+        [ChildActionOnly]
+        public ActionResult TimeWithParam(DateTime time)
+        {
+            return PartialView(time);
+        }
+    }
+}
--- WorkingWithRazor/Global.asax.cs
+++ WorkingWithRazor/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using WorkingWithRazor.Infrastructure;
 
 namespace WorkingWithRazor
 {
@@ -13,6 +14,10 @@ namespace WorkingWithRazor
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            // to use my own view engine
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new CustomLocationViewEngine());
         }
     }
 }
